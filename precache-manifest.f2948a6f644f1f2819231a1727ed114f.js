e12dcbea8fcb1183ab7a45f86253b47f9c920713
mikaellapcns <2014068497@ust-ics.mygbiz.com>
2021-01-31 22:37:15 +0800

    Updates

---
+++ precache-manifest.f2948a6f644f1f2819231a1727ed114f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "788e7189f8dc1f689c4f8d381fa6a44b",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "260c9fc69419154716e9",
+    "url": "/react-weather/static/css/main.c65ace98.chunk.css"
+  },
+  {
+    "revision": "13530e00ad45050580a0",
+    "url": "/react-weather/static/js/2.f2d6ca46.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/react-weather/static/js/2.f2d6ca46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "260c9fc69419154716e9",
+    "url": "/react-weather/static/js/main.f0feea01.chunk.js"
+  },
+  {
+    "revision": "f638013bd88fa5b26bac",
+    "url": "/react-weather/static/js/runtime-main.b9e6359b.js"
+  },
+  {
+    "revision": "4dc8014edb976cc56a8c463f62522509",
+    "url": "/react-weather/static/media/cloudy.4dc8014e.png"
+  },
+  {
+    "revision": "fa9c49ba7e36ff07aa6d1e88c86d1eec",
+    "url": "/react-weather/static/media/cumulus.fa9c49ba.png"
+  },
+  {
+    "revision": "597be5d618f33020fe2e5420104c80d6",
+    "url": "/react-weather/static/media/moderaterain.597be5d6.png"
+  },
+  {
+    "revision": "32ebe57aa1b4c86302a680437c62a7a4",
+    "url": "/react-weather/static/media/rain.32ebe57a.png"
+  },
+  {
+    "revision": "d10b6c70766463b300d8d3e43aab104d",
+    "url": "/react-weather/static/media/shower-rain.d10b6c70.png"
+  },
+  {
+    "revision": "a8b691228922f2d2aa70036d5a5b0646",
+    "url": "/react-weather/static/media/snow.a8b69122.png"
+  },
+  {
+    "revision": "604ea2fbbaa860b8427357e0d996ccc4",
+    "url": "/react-weather/static/media/stratus.604ea2fb.png"
+  },
+  {
+    "revision": "70b116eea9cbdcd1d5a0d4063bc62324",
+    "url": "/react-weather/static/media/stratuscumulus.70b116ee.png"
+  },
+  {
+    "revision": "ff33f0de4f88c23349289bcb5798cd42",
+    "url": "/react-weather/static/media/sun.ff33f0de.png"
+  },
+  {
+    "revision": "3dec59430708f76bbf5fef40f0159ac7",
+    "url": "/react-weather/static/media/thunder.3dec5943.png"
+  }
+]);
